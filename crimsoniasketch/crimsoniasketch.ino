f6b8ccfb96b4ab36995bf6e550c0afbc8fd49956
foolyyy <122584056+bexplayer@users.noreply.github.com>
2023-01-23 09:20:49 -0500

    Initial Files
    commit initial files

---
+++ crimsoniasketch/crimsoniasketch.ino
@@ -0,0 +1,20 @@
+// include headers
+#include <package.h>
+
+// variable declarations
+int pins = {1, 2, 3};
+
+// config handler function
+void config(){
+
+}
+
+// setup function
+void setup(){
+  Serial.begin(9600);
+}
+
+// setup loop
+void loop(){
+
+}
